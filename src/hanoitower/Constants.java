8fa85d3bee185a4cd67ede41935887c14ce66a78
lilitadamyan <lilitadamyan2015@gmail.com>
2016-05-21 00:38:32 +0400

    Created and designed pegs and  added textfield for input number of discs

---
+++ src/hanoitower/Constants.java
@@ -0,0 +1,14 @@
+package hanoitower;
+
+/**
+ * Created by 1 on 20.05.2016.
+ */
+public class Constants {
+
+    static final int DISC_HEIGHT = 15;
+    static final int DISC_WIDTH= 15;
+    static final int PEG_SPACE = 100;
+
+
+
+}
