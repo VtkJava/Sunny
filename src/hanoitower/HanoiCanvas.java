5ed7305864b83784597e7d2eb9abe7e299b09e38
lilitadamyan <lilitadamyan2015@gmail.com>
2016-05-21 01:05:11 +0400

    rename package

--- src/hanoitower/HanoiCanvas.java
+++ src/hanoitower/HanoiCanvas.java
@@ -14,22 +14,23 @@ public class HanoiCanvas extends FigureCanvas implements Runnable {
     public HanoiCanvas() {
     }
 
+
     @Override
     public void paint(Graphics g) {
         super.paint(g);
 
         g.setColor(Color.GRAY);
         g.fillRect(150, 67, 17, 270);
-        g.fillRect(510,335, 220, 20);
+        g.fillRect(510, 335, 230, 20);
 
         g.fillRect(385, 67, 17, 270);
-        g.fillRect(280,335,220,20);
+        g.fillRect(280, 335, 220, 20);
 
         g.fillRect(620, 67, 17, 270);
         g.fillRect(50, 335, 220, 20);
     }
 
-    public void moveDisc(Peg peg1,Peg peg2) {
+    public void moveDisc(Peg peg1, Peg peg2) {
 //mode
     }
 
