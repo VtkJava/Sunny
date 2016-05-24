9024cdda642891bd48f53c0625c7f94db240f6da
lilitadamyan <lilitadamyan2015@gmail.com>
2016-05-24 22:29:36 +0400

    added constants

--- src/hanoitower/Constants.java
+++ src/hanoitower/Constants.java
@@ -2,11 +2,10 @@ package hanoitower;
 
 public class Constants {
 
-    static final int UP =30;
-    static final int TODESTINATION= 15;
-    static final int DOWN = 100;
-    static final int DONE = 100;
-
+    private static final int UP = 30;
+    private static final int TODESTINATION = 15;
+    private static final int DOWN = 100;
+    private static final int DONE = 100;
 
 
 }
